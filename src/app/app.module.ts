5685eb13c331e5c488b17211e644f1637ba40319
Saul Salcedo Balderas <saulsalbal@gmail.com>
2020-11-28 00:25:07 -0600

    Carga inicial del proyecto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { XboxComponent } from './components/sections/xbox/xbox.component';
+import { PlayStationComponent } from './components/sections/play-station/play-station.component';
+import { PCComponent } from './components/sections/pc/pc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    XboxComponent,
+    PlayStationComponent,
+    PCComponent
   ],
   imports: [
     BrowserModule,
